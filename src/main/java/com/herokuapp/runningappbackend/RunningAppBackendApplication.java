14ca0a97f081f0568643ff51a20a5731c4de0882
Marcin Sitkiewicz <sitkiewicz.m.2f@gmail.com>
2021-10-16 14:39:55 +0200

    initial commit

---
+++ src/main/java/com/herokuapp/runningappbackend/RunningAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.runningappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/herokuapp/runningappbackend/RunningAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herokuapp.runningappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
