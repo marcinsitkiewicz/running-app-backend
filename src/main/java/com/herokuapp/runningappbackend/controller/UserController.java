ff40e7abc9958454fdfbf51695637777129c1fba
Marcin Sitkiewicz <sitkiewicz.m.2f@gmail.com>
2021-10-17 17:23:45 +0200

    updated UserController to implement newly added UserService

--- src/main/java/com/herokuapp/runningappbackend/controller/UserController.java
+++ src/main/java/com/herokuapp/runningappbackend/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.herokuapp.runningappbackend.controller;
 
+import com.herokuapp.runningappbackend.dto.UserDTO;
 import com.herokuapp.runningappbackend.model.User;
 import com.herokuapp.runningappbackend.repository.UserRepository;
+import com.herokuapp.runningappbackend.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,22 +12,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserServiceImpl userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers() {
+    public ResponseEntity<Collection<UserDTO>> getAllUsers() {
         try {
-            List<User> users = new ArrayList<>(userRepository.findAll());
+            Collection<UserDTO> users = userService.getAll();
 
             if (users.isEmpty()) {
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
