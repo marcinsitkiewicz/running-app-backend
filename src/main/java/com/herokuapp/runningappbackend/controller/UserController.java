516d007194cbdf92666b4e0139c441f7eaa371bf
Marcin Sitkiewicz <sitkiewicz.m.2f@gmail.com>
2021-10-17 17:28:59 +0200

    optimized imports

--- src/main/java/com/herokuapp/runningappbackend/controller/UserController.java
+++ src/main/java/com/herokuapp/runningappbackend/controller/UserController.java
@@ -1,19 +1,14 @@
 package com.herokuapp.runningappbackend.controller;
 
 import com.herokuapp.runningappbackend.dto.UserDTO;
-import com.herokuapp.runningappbackend.model.User;
-import com.herokuapp.runningappbackend.repository.UserRepository;
 import com.herokuapp.runningappbackend.service.UserServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api")
