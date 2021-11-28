a7d9ce0906663ea0b9ab8814ce9ba2f68638ebf8
Marcin Sitkiewicz <sitkiewicz.m.2f@gmail.com>
2021-11-28 19:33:13 +0100

    created implementations of UserDetails and UserDetailsService

---
+++ src/main/java/com/herokuapp/runningappbackend/service/UserDetailsImpl.java
@@ -0,0 +1,66 @@
+package com.herokuapp.runningappbackend.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.herokuapp.runningappbackend.model.User;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    public UserDetailsImpl(Long id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
